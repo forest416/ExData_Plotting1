c3312e310c114106ec9de1146cbe9f485908102e
forest416 <forest416@gmail.com>
2015-08-05 00:23:24 -0400

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,27 @@
+cons$d = as.Date((cons$Date), '%d/%m/%Y' )
+d.filter = as.Date(c('2007-02-01','2007-02-02'))
+
+cons.work = (cons[cons$d %in% d.filter,])
+
+#cons.work$Time = strptime(cons.work$Time, format = '%T',tz='')
+cons.work$dt = strptime(paste(cons.work$Date, cons.work$Time) ,'%d/%m/%Y %T')
+cons.work$Date = NULL
+cons.work$Time = NULL
+cons.work$d = NULL
+
+cons.work$Global_active_power = as.numeric(cons.work$Global_active_power)
+cons.work$Global_reactive_power = as.numeric(cons.work$Global_reactive_power)
+cons.work$Voltage = as.numeric(cons.work$Voltage)
+cons.work$Global_intensity = as.numeric(cons.work$Global_intensity)
+cons.work$Sub_metering_1 = as.numeric(cons.work$Sub_metering_1)
+cons.work$Sub_metering_2 = as.numeric(cons.work$Sub_metering_2)
+cons.work$Sub_metering_3 = as.numeric(cons.work$Sub_metering_3)
+
+par(mfrow=c(1,1))
+par(mfcol=c(1,1))
+#Plot 1
+
+png(filename = 'Plot1.png', width = 480, height = 480, units = 'px')
+hist(cons.work$Global_active_power, col='red', xlab='Global Activie Power(kilowatts)', ylab = 'Frequency', main = 'Global Active Power')
+dev.off()
+
